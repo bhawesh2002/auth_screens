df5b623716114af3b279fb6c71e724e796d7d6cc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-03 19:47:07 +0530

    chore: Update login page to use layout builder to layout widgets

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,17 +5,41 @@ class LogIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
         body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade100,
-          ),
-          child: const Text("Login Page"),
-        ),
+            width: width,
+            height: height,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+            ),
+            margin: EdgeInsets.symmetric(
+              horizontal: width * 0.03,
+              vertical: height * 0.015,
+            ),
+            child: LayoutBuilder(builder: (context, constraints) {
+              return Stack(
+                children: [
+                  Positioned.fill(
+                    top: height * 0.125,
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.grey.shade900,
+                          fontSize: width * 0.08,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              );
+            })),
       ),
     );
   }
