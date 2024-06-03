1f5273b5c85e0c0990bfa0d47a8b40a22a30f960
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-03 15:14:50 +0530

    feat: Add SignUp page to the app

---
+++ lib/pages/signup.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade100,
+          ),
+          child: const Text("Signup Page"),
+        ),
+      ),
+    );
+  }
+}
