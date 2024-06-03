4b5fcfa301589de1789ff40fa76d08142813377c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-03 15:14:56 +0530

    feat: Add SignUp button to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:auth_screens/pages/signup.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,15 +6,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
       body: Center(
-        child: Text(
-          "Auth Screen Example",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Auth Screen Example",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox.square(
+              dimension: 16,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SignUp(),
+                  ),
+                );
+              },
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 16,
+                ),
+              ),
+              child: Text(
+                "Signup Page",
+                style: TextStyle(color: Colors.grey.shade900, fontSize: 16),
+              ),
+            )
+          ],
         ),
       ),
     ));
