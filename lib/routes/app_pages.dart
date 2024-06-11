59cfab96174d118aa7126350f2c0a93aadd0970b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-11 11:10:13 +0530

    refactor: correct the page property of GetPage by using  WelcomePage

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:auth_screens/pages/home.dart';
 import 'package:auth_screens/pages/login.dart';
 import 'package:auth_screens/pages/signup.dart';
+import 'package:auth_screens/pages/welcome.dart';
 import 'package:auth_screens/routes/app_routes.dart';
 import 'package:get/get.dart';
 
@@ -9,6 +10,6 @@ class AppPages {
     GetPage(name: AppRoutes.home, page: () => const HomePage()),
     GetPage(name: AppRoutes.login, page: () => const LogIn()),
     GetPage(name: AppRoutes.signup, page: () => SignUp()),
-    GetPage(name: AppRoutes.welcome, page: () => const HomePage())
+    GetPage(name: AppRoutes.welcome, page: () => const WelcomePage())
   ];
 }
