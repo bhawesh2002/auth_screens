354ffdd23d96056557feaa66f4847dbee9e58564
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-18 14:13:24 +0530

    refactor: Update AuthStateController to handle user authentication and account management

---
+++ lib/controllers/auth_state_controller.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthStateController extends GetxController {
+  final _authInstance = FirebaseAuth.instance;
+  final Rx<User?> _user = Rx<User?>(null);
+
+  @override
+  void onReady() {
+    super.onReady();
+    _authState();
+  }
+
+  void _authState() {
+    _user.value = _authInstance.currentUser;
+    if (_user.value == null) {
+      debugPrint("User is logged out");
+    } else {
+      debugPrint("${_user.value?.email} is logged in");
+    }
+  }
+
+  Future<void> signUpWithEmailandPass(
+      {required String email, required String password}) async {
+    try {
+      await _authInstance.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      debugPrint("signUpWithEmailandPass(FirebaseAuthException):$e");
+    } catch (e) {
+      debugPrint("signUpWithEmailandPass:$e");
+    }
+  }
+
+  Future<void> loginWithEmailandPass(
+      {required String email, required String password}) async {
+    try {
+      await _authInstance.signInWithEmailAndPassword(
+          email: email, password: password);
+      _authState();
+    } on FirebaseAuthException catch (e) {
+      debugPrint("loginWithEmailandPass(FirebaseAuthException):$e");
+    } catch (e) {
+      debugPrint("loginWithEmailandPass:$e");
+    }
+  }
+
+  Future<void> signOut() async {
+    await _authInstance.signOut();
+    _user.value == null;
+  }
+}
