b6143aa8c04071064eab45d3a511f24148fa1591
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-20 10:56:56 +0530

    feat: Align the container to the top

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -23,171 +23,177 @@ class _SignupPageState extends State<SignupPage> {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Container(
-            width: Get.width * 0.9,
-            height: Get.height * 0.5,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(Get.width * 0.025),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, Get.height * 0.010),
-                  color: Colors.grey.shade300,
-                  blurRadius: 30,
-                  spreadRadius: 10,
-                )
-              ],
-            ),
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                return Stack(
-                  children: [
-                    Positioned.fill(
-                      bottom: constraints.maxHeight * 0.6,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                          width: constraints.maxWidth * 0.9,
-                          child: TextField(
-                            controller: _emailController,
-                            cursorColor: Colors.blue.shade600,
-                            decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              hintText: "Email",
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.blue.shade600,
-                                  width: 2,
-                                ),
-                              ),
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.black12,
-                                  width: 1,
-                                ),
-                              ),
-                              errorBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.redAccent,
-                                  width: 1,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned.fill(
-                      bottom: constraints.maxHeight * 0.2,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                          width: constraints.maxWidth * 0.9,
-                          child: TextField(
-                            controller: _passController,
-                            obscureText: true,
-                            cursorColor: Colors.blue.shade600,
-                            decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              hintText: "Password",
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.blue.shade600,
-                                  width: 2,
-                                ),
-                              ),
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.black12,
-                                  width: 1,
-                                ),
-                              ),
-                              errorBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.redAccent,
-                                  width: 1,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned.fill(
-                      top: constraints.maxHeight * 0.2,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: SizedBox(
-                          width: constraints.maxWidth * 0.9,
-                          child: TextField(
-                            controller: _confirmPassController,
-                            cursorColor: Colors.blue.shade600,
-                            decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              hintText: "Confirm Password",
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.blue.shade600,
-                                  width: 2,
-                                ),
-                              ),
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.black12,
-                                  width: 1,
-                                ),
-                              ),
-                              errorBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.redAccent,
-                                  width: 1,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned.fill(
-                      top: constraints.maxHeight * 0.6,
-                      child: Align(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: Get.width * 0.9,
+              height: Get.height * 0.5,
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.1),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(Get.width * 0.025),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, Get.height * 0.010),
+                    color: Colors.grey.shade300,
+                    blurRadius: 30,
+                    spreadRadius: 10,
+                  )
+                ],
+              ),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Stack(
+                    children: [
+                      Positioned.fill(
+                        bottom: constraints.maxHeight * 0.6,
+                        child: Align(
                           alignment: Alignment.center,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(
-                              constraints.maxWidth * 0.02,
-                            ),
-                            color: Colors.blue.shade600,
-                            clipBehavior: Clip.antiAlias,
-                            child: InkWell(
-                              splashColor: const Color.fromARGB(31, 0, 0, 0),
-                              highlightColor: const Color.fromARGB(31, 0, 0, 0),
-                              onTap: () async {
-                                await _authStateController
-                                    .signUpWithEmailandPass(
-                                        email: _emailController.text,
-                                        password: _passController.text);
-                                _emailController.clear();
-                                _passController.clear();
-                                _confirmPassController.clear();
-                              },
-                              child: Container(
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: constraints.maxWidth * 0.12,
-                                    vertical: constraints.maxHeight * 0.03),
-                                child: const Text(
-                                  "Signup",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                          child: SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: TextField(
+                              controller: _emailController,
+                              cursorColor: Colors.blue.shade600,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                hintText: "Email",
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.blue.shade600,
+                                    width: 2,
+                                  ),
+                                ),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black12,
+                                    width: 1,
+                                  ),
+                                ),
+                                errorBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.redAccent,
+                                    width: 1,
                                   ),
                                 ),
                               ),
                             ),
-                          )),
-                    ),
-                  ],
-                );
-              },
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        bottom: constraints.maxHeight * 0.2,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: TextField(
+                              controller: _passController,
+                              obscureText: true,
+                              cursorColor: Colors.blue.shade600,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                hintText: "Password",
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.blue.shade600,
+                                    width: 2,
+                                  ),
+                                ),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black12,
+                                    width: 1,
+                                  ),
+                                ),
+                                errorBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.redAccent,
+                                    width: 1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        top: constraints.maxHeight * 0.2,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: TextField(
+                              controller: _confirmPassController,
+                              cursorColor: Colors.blue.shade600,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                hintText: "Confirm Password",
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.blue.shade600,
+                                    width: 2,
+                                  ),
+                                ),
+                                enabledBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black12,
+                                    width: 1,
+                                  ),
+                                ),
+                                errorBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.redAccent,
+                                    width: 1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        top: constraints.maxHeight * 0.6,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(
+                                constraints.maxWidth * 0.02,
+                              ),
+                              color: Colors.blue.shade600,
+                              clipBehavior: Clip.antiAlias,
+                              child: InkWell(
+                                splashColor: const Color.fromARGB(31, 0, 0, 0),
+                                highlightColor:
+                                    const Color.fromARGB(31, 0, 0, 0),
+                                onTap: () async {
+                                  await _authStateController
+                                      .signUpWithEmailandPass(
+                                          email: _emailController.text,
+                                          password: _passController.text);
+                                  _emailController.clear();
+                                  _passController.clear();
+                                  _confirmPassController.clear();
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: constraints.maxWidth * 0.12,
+                                      vertical: constraints.maxHeight * 0.03),
+                                  child: const Text(
+                                    "Signup",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
           ),
         ),
