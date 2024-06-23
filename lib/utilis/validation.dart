bcc82f0bb6d0579a95d8eaf747a17d55db709c6a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-23 21:11:07 +0530

    feat: Add email validation and password requirements to validation.dart

---
+++ lib/utilis/validation.dart
@@ -0,0 +1,38 @@
+bool validateEmail(String email) {
+  final RegExp emailCheck = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+
+  return emailCheck.hasMatch(email);
+}
+
+String validatePassword(String password) {
+  // Individual regular expressions for each password requirement
+  final RegExp hasUpperCase = RegExp(r'(?=.*[A-Z])');
+  final RegExp hasLowerCase = RegExp(r'(?=.*[a-z])');
+  final RegExp hasDigit = RegExp(r'(?=.*\d)');
+  final RegExp hasSpecialCharacter = RegExp(r'(?=.*[!@#$%^&*(),.?":{}|<>])');
+  final RegExp hasMinLength = RegExp(r'.{8,}');
+
+  // List to hold feedback messages
+  List<String> feedback = [];
+
+  // Check each requirement and add feedback if missing
+  if (!hasUpperCase.hasMatch(password)) {
+    feedback.add('at least one uppercase letter');
+  }
+  if (!hasLowerCase.hasMatch(password)) {
+    feedback.add('at least one lowercase letter');
+  }
+  if (!hasDigit.hasMatch(password)) {
+    feedback.add('at least one digit');
+  }
+  if (!hasSpecialCharacter.hasMatch(password)) {
+    feedback.add('at least one special character');
+  }
+  if (!hasMinLength.hasMatch(password)) {
+    feedback.add('at least 8 characters long');
+  }
+
+  return feedback.join('\n ');
+}
