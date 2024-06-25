d0ddb898b1dc77267f33d20bbfd47ea913c294dc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-25 11:31:12 +0530

    refactor: Change SignupPage Layout by using Columns

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -58,179 +58,174 @@ class _SignupPageState extends State<SignupPage> {
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Column(children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              width: Get.width * 0.9,
-              height: Get.height * 0.5,
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.1),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade100,
-                borderRadius: BorderRadius.circular(Get.width * 0.025),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, Get.height * 0.010),
-                    color: Colors.grey.shade300,
-                    blurRadius: 30,
-                    spreadRadius: 10,
-                  )
-                ],
-              ),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Form(
-                    key: _validationKey,
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                          bottom: constraints.maxHeight * 0.6,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: SizedBox(
-                              width: constraints.maxWidth * 0.9,
-                              child: EmailTextField(
-                                emailController: _emailController,
-                                emailFocusNode: _emailFocusNode,
-                                validator: emailValidator,
-                              ),
+        body: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: Get.width * 0.9,
+                // height: Get.height * 0.6,
+                //margin: EdgeInsets.only(
+                //    top: MediaQuery.of(context).size.height * 0.1),
+                padding: EdgeInsets.symmetric(vertical: Get.height * 0.025),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(Get.width * 0.025),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, Get.height * 0.010),
+                      color: Colors.grey.shade300,
+                      blurRadius: 30,
+                      spreadRadius: 10,
+                    )
+                  ],
+                ),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Form(
+                      key: _validationKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: EmailTextField(
+                              emailController: _emailController,
+                              emailFocusNode: _emailFocusNode,
+                              validator: emailValidator,
                             ),
                           ),
-                        ),
-                        Positioned.fill(
-                          bottom: constraints.maxHeight * 0.2,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: SizedBox(
-                              width: constraints.maxWidth * 0.9,
-                              child: PasswordTextField(
-                                passController: _passController,
-                                passFocusNode: _passFocusNode,
-                                passVisible: _passVisible,
-                                validator: passwordValidator,
-                              ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: Get.height * 0.02,
                             ),
                           ),
-                        ),
-                        Positioned.fill(
-                          top: constraints.maxHeight * 0.2,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: SizedBox(
-                              width: constraints.maxWidth * 0.9,
-                              child: PasswordTextField(
-                                passController: _confirmPassController,
-                                passFocusNode: _confirmPassFocusNode,
-                                passVisible: _conirfPassVisible,
-                                hintText: "Confirm Password",
-                                validator: confirmPasswordValidator,
-                              ),
+                          SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: PasswordTextField(
+                              passController: _passController,
+                              passFocusNode: _passFocusNode,
+                              passVisible: _passVisible,
+                              validator: passwordValidator,
                             ),
                           ),
-                        ),
-                        Positioned.fill(
-                          top: constraints.maxHeight * 0.6,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(
-                                constraints.maxWidth * 0.02,
-                              ),
-                              color: Colors.blue.shade600,
-                              clipBehavior: Clip.antiAlias,
-                              child: InkWell(
-                                splashColor: const Color.fromARGB(31, 0, 0, 0),
-                                highlightColor:
-                                    const Color.fromARGB(31, 0, 0, 0),
-                                onTap: () async {
-                                  setState(() {
-                                    signUpTapped = true;
-                                  });
-                                  _validationKey.currentState?.validate();
-                                  if (_emailController.text.isNotEmpty) {
-                                    if (_passController.text.isNotEmpty) {
-                                      if (_confirmPassController
-                                              .text.isNotEmpty &&
-                                          _passController.text ==
-                                              _confirmPassController.text) {
-                                        await _authStateController
-                                            .signUpWithEmailandPass(
-                                                email: _emailController.text,
-                                                password: _passController.text);
-                                      } else {
-                                        _confirmPassFocusNode.requestFocus();
-                                      }
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: Get.height * 0.02,
+                            ),
+                          ),
+                          SizedBox(
+                            width: constraints.maxWidth * 0.9,
+                            child: PasswordTextField(
+                              passController: _confirmPassController,
+                              passFocusNode: _confirmPassFocusNode,
+                              passVisible: _conirfPassVisible,
+                              hintText: "Confirm Password",
+                              validator: confirmPasswordValidator,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              bottom: Get.height * 0.06,
+                            ),
+                          ),
+                          Material(
+                            borderRadius: BorderRadius.circular(
+                              constraints.maxWidth * 0.02,
+                            ),
+                            color: Colors.blue.shade600,
+                            clipBehavior: Clip.antiAlias,
+                            child: InkWell(
+                              splashColor: const Color.fromARGB(31, 0, 0, 0),
+                              highlightColor: const Color.fromARGB(31, 0, 0, 0),
+                              onTap: () async {
+                                setState(() {
+                                  signUpTapped = true;
+                                });
+                                _validationKey.currentState?.validate();
+                                if (_emailController.text.isNotEmpty) {
+                                  if (_passController.text.isNotEmpty) {
+                                    if (_confirmPassController
+                                            .text.isNotEmpty &&
+                                        _passController.text ==
+                                            _confirmPassController.text) {
+                                      await _authStateController
+                                          .signUpWithEmailandPass(
+                                              email: _emailController.text,
+                                              password: _passController.text);
                                     } else {
-                                      _passFocusNode.requestFocus();
+                                      _confirmPassFocusNode.requestFocus();
                                     }
                                   } else {
-                                    _emailFocusNode.requestFocus();
+                                    _passFocusNode.requestFocus();
                                   }
-                                  Future.delayed(
-                                    const Duration(seconds: 1),
-                                    () {
-                                      setState(() {
-                                        signUpTapped = false;
-                                      });
-                                    },
-                                  );
-                                  if (_authStateController.user != null) {
-                                    _validationKey.currentState!.reset();
-                                  }
-                                },
-                                child: SizedBox(
-                                  width: constraints.maxWidth * 0.4,
-                                  height: constraints.maxHeight * 0.12,
-                                  child: Center(
-                                    child: signUpTapped
-                                        ? SizedBox.square(
-                                            dimension:
-                                                constraints.maxWidth * 0.05,
-                                            child:
-                                                const CircularProgressIndicator(
-                                              color: Colors.white,
-                                            ),
-                                          )
-                                        : const Text(
-                                            "Signup",
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                            ),
+                                } else {
+                                  _emailFocusNode.requestFocus();
+                                }
+                                Future.delayed(
+                                  const Duration(seconds: 1),
+                                  () {
+                                    setState(() {
+                                      signUpTapped = false;
+                                    });
+                                  },
+                                );
+                                if (_authStateController.user != null) {
+                                  _validationKey.currentState!.reset();
+                                }
+                              },
+                              child: SizedBox(
+                                width: Get.width * 0.35,
+                                height: Get.height * 0.065,
+                                child: Center(
+                                  child: signUpTapped
+                                      ? SizedBox.square(
+                                          dimension:
+                                              constraints.maxWidth * 0.05,
+                                          child:
+                                              const CircularProgressIndicator(
+                                            color: Colors.white,
                                           ),
-                                  ),
+                                        )
+                                      : const Text(
+                                          "Signup",
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: Get.height * 0.1,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.offAll(() => const LoginPage());
                 },
-              ),
-            ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.blue.shade600,
+                    fontSize: 20,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
           ),
-          SizedBox.square(
-            dimension: Get.width * 0.3,
-          ),
-          GestureDetector(
-            onTap: () {
-              Get.offAll(() => const LoginPage());
-            },
-            child: Text(
-              "Login",
-              style: TextStyle(
-                color: Colors.blue.shade600,
-                fontSize: 20,
-                letterSpacing: 1,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          )
-        ]),
+        ),
       ),
     );
   }
