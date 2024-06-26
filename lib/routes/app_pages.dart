743812c4c781973b97e9a0dc368fc10e95035eaa
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-26 18:53:25 +0530

    feat: Add AuthOptionsPage, LoginPage, and AuthOptionsPage to app routes

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,3 +1,5 @@
+import 'package:auth_screens/pages/auth_option_grid.dart';
+import 'package:auth_screens/pages/login_page.dart';
 import 'package:auth_screens/pages/signup_page.dart';
 import 'package:auth_screens/routes/app_routes.dart';
 import 'package:get/get.dart';
@@ -7,6 +9,14 @@ class AppPages {
     GetPage(
       name: AppRoutes.signup,
       page: () => const SignupPage(),
+    ),
+    GetPage(
+      name: AppRoutes.login,
+      page: () => const LoginPage(),
+    ),
+    GetPage(
+      name: AppRoutes.authopt,
+      page: () => const AuthOptionsPage(),
     )
   ];
 }
